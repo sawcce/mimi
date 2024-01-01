ad678a7c084e0d03c1b24a167d68cc4aa0308ad4
alexis <sawcce@proton.me>
2024-01-01 20:20:52 +0100

    main: missing line break

--- kernel/src/main.zig
+++ kernel/src/main.zig
@@ -33,7 +33,7 @@ export fn _start() callconv(.C) noreturn {
     }
 
     Module.init_modules(&Modules);
-    Procedures.write_message("Modules init!");
+    Procedures.write_message("Modules init!\n");
 
     // We're done, just hang...
     done();
