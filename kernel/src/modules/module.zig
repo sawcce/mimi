2cf35af5bce846ab40b630e34a64d693dd02fd6a
alexis <sawcce@proton.me>
2024-01-01 19:34:34 +0100

    rename module specification struct, made init/deinit nullable

--- kernel/src/modules/module.zig
+++ kernel/src/modules/module.zig
@@ -1,4 +1,4 @@
-pub const Module = struct {
-    init: *const fn () void,
-    deinit: *const fn () void,
+pub const ModuleSpec = struct {
+    init: ?*const fn () void,
+    deinit: ?*const fn () void,
 };
