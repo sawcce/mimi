a9ac90b2e6bedd2cb130fc36956344f10dbaef80
alexis <sawcce@proton.me>
2024-01-01 20:57:51 +0100

    exceptions: added exceptions module

---
+++ kernel/src/modules/exceptions.zig
@@ -0,0 +1,23 @@
+const ModuleSpec = @import("module.zig").ModuleSpec;
+const Interrupts = @import("interrupts.zig");
+const Procedures = @import("procedures.zig");
+
+const std = @import("std");
+
+pub const Module = ModuleSpec{
+    .init = init,
+    .deinit = null,
+};
+
+pub fn init() void {
+    for (0..0xF) |i| {
+        Interrupts.add_interrupt(@intCast(i), default_handler);
+    }
+
+    Interrupts.load();
+}
+
+/// Only to be used on known interrupts
+pub fn default_handler(frame: *Interrupts.Frame) void {
+    Procedures.write_fmt("Interrupt: {s} invoked\n", .{Interrupts.name(frame.idx)}) catch {};
+}
