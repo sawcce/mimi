19a7eabfc0f8af406ebb16ea479e58789050a889
alexis <sawcce@proton.me>
2024-01-01 19:39:13 +0100

    module: added todo note

--- kernel/src/modules/module.zig
+++ kernel/src/modules/module.zig
@@ -3,6 +3,8 @@ pub const ModuleSpec = struct {
     deinit: ?*const fn () void,
 };
 
+// TODO: Keep track of loaded modules and support for deinit
+
 pub fn init_modules(modules: []ModuleSpec) void {
     for (modules) |module| {
         module.init();
