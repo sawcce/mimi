2db227b2bd04ea0475793a8e611b32352679f8be
alexis <sawcce@proton.me>
2024-01-01 19:37:54 +0100

    module: added init_modules

--- kernel/src/modules/module.zig
+++ kernel/src/modules/module.zig
@@ -2,3 +2,9 @@ pub const ModuleSpec = struct {
     init: ?*const fn () void,
     deinit: ?*const fn () void,
 };
+
+pub fn init_modules(modules: []ModuleSpec) void {
+    for (modules) |module| {
+        module.init();
+    }
+}
