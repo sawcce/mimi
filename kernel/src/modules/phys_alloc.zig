368a57c667b749744c38877c003727dab90a4442
alexis <sawcce@proton.me>
2024-01-02 19:41:00 +0100

    phys_alloc: added physical memory allocator module

---
+++ kernel/src/modules/phys_alloc.zig
@@ -0,0 +1,61 @@
+const ModuleSpec = @import("module.zig").ModuleSpec;
+const Procedures = @import("procedures.zig");
+
+const Limine = @import("limine");
+const std = @import("std");
+
+pub export var MemoryMapRequest: Limine.MemoryMapRequest = .{};
+
+pub const Module = ModuleSpec{
+    .init = init,
+    .deinit = null,
+};
+
+pub const PageSize: u64 = 4096;
+
+const PageAllocator = struct {
+    page_amount: u64,
+    bitmap: [*]u8,
+    frames: *anyopaque,
+};
+
+pub var Allocator: ?*PageAllocator = null;
+
+var Bitmap: []u32 = undefined;
+
+pub fn init() void {
+    if (MemoryMapRequest.response) |memory_map| {
+        var biggest_entry: ?*Limine.MemoryMapEntry = null;
+
+        for (0..memory_map.entry_count) |i| {
+            const entry = memory_map.entries_ptr[i];
+
+            if (entry.kind == Limine.MemoryMapEntryType.usable and (biggest_entry == null or entry.length > biggest_entry.?.length)) {
+                biggest_entry = entry;
+            }
+        }
+
+        if (biggest_entry == null) {
+            Procedures.write_fmt("Couldn't find any usable piece of memory\n", .{}) catch {};
+            return;
+        }
+
+        // Equation for the amount of pages available (n) for a set amount of memory (t) and page size (p):
+        // t = n/8 + pn
+        // n/8 corresponds to the size of the bitmap as one byte can store information about 8 pages
+        // you can then find the amount of pages:
+        // n = 8t/(1+8p)
+        const t: f64 = @floatFromInt(biggest_entry.?.length);
+        const n: f64 = 8 * t / @as(f64, @floatCast(1.0 + 8.0 * @as(f64, @floatFromInt(PageSize))));
+        const amount_of_pages: u64 = @intFromFloat(@floor(n));
+
+        const allocator_base = std.mem.alignForward(u64, biggest_entry.?.base, PageSize);
+        const frames_base = std.mem.alignForward(u64, allocator_base + amount_of_pages, PageSize);
+        const end = std.mem.alignBackward(u64, biggest_entry.?.base + biggest_entry.?.length, PageSize);
+        const new_page_amount = (end - frames_base) / 4096;
+
+        Allocator = @ptrCast(allocator_base);
+        Allocator.?.page_amount = new_page_amount;
+        Allocator.?.frames = frames_base;
+    }
+}
