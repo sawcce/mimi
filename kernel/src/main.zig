c103b853c4dcb3ac48aaaac5bb38c856c344e3e5
Sawcce <sawcce@proton.me>
2024-01-04 16:11:07 +0100

    support for main function, keeping track of modules

--- kernel/src/main.zig
+++ kernel/src/main.zig
@@ -35,13 +35,22 @@ export fn _start() callconv(.C) noreturn {
         done();
     }
 
-    if(PhysAlloc.Module.init) |init| init();
-
-    Module.init_modules(&Modules);
-    Procedures.write_message("Modules successfully initialized!\n");
+    main() catch |e| {
+        Procedures.write_fmt("Error: {}", .{e}) catch {};
+    };
 
     // We're done, just hang...
     done();
+}
+
+fn main() !void {
+    if (PhysAlloc.Module.init) |init| init();
+
+    Module.init();
+    try Module.loaded_modules.append(PhysAlloc.Module);
+
+    try Module.init_modules(&Modules);
+    Procedures.write_message("Modules successfully initialized!\n");
 }
 
 pub fn panic(msg: []const u8, error_return_trace: ?*std.builtin.StackTrace, e: ?usize) noreturn {
--- kernel/src/modules/module.zig
+++ kernel/src/modules/module.zig
@@ -1,4 +1,7 @@
 const Procedures = @import("procedures.zig");
+const PhysAlloc = @import("phys_alloc.zig");
+
+const std = @import("std");
 
 pub const ModuleSpec = struct {
     name: []const u8,
@@ -6,12 +9,19 @@ pub const ModuleSpec = struct {
     deinit: ?*const fn () void,
 };
 
-// TODO: Keep track of loaded modules and support for deinit
+pub var loaded_modules: std.ArrayList(ModuleSpec) = undefined;
 
-pub fn init_modules(modules: []const ModuleSpec) void {
+// TODO: Support for deinit
+
+pub fn init() void {
+    loaded_modules = std.ArrayList(ModuleSpec).init(PhysAlloc.allocator());
+}
+
+pub fn init_modules(modules: []const ModuleSpec) !void {
     for (modules) |module| {
-        if (module.init) |init| {
-            init();
+        if (module.init) |init_| {
+            init_();
+            try loaded_modules.append(module);
             Procedures.write_fmt("Module {s} successfully initialized\n", .{module.name}) catch {};
         }
     }
