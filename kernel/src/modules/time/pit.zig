a0d9e572e35b3d4e4ec0a80411f14d1bf9cc6e15
Sawcce <sawcce@proton.me>
2024-01-07 21:23:56 +0100

    pit: added ability to change pit counter

--- kernel/src/modules/time/pit.zig
+++ kernel/src/modules/time/pit.zig
@@ -1,7 +1,10 @@
 const Ports = @import("../ports.zig");
+const Procedures = @import("../procedures.zig");
+
+pub var pit_ticks: u64 = 0;
 
 pub fn initPIT() void {
-    // const pit_counter = Ports.Port(u8).new(0x40);
+    const pit_counter = Ports.Port(u8).new(0x40);
     const pit_cmd = Ports.Port(u8).new(0x43);
 
     const binary_mode = 0x0;
@@ -10,4 +13,10 @@ pub fn initPIT() void {
     const mode_square_wave = 0x06;
 
     pit_cmd.write(channel_select | access_mode_low_high | mode_square_wave | binary_mode);
+    pit_counter.write(0);
+}
+
+pub fn wait(duration: u32) void {
+    const start = pit_ticks;
+    while (pit_ticks - start < duration) {}
 }
