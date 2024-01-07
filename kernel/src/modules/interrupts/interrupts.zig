74044e084fe1b3ae48be7f5e1061405a82b2d461
Sawcce <sawcce@proton.me>
2024-01-07 16:06:04 +0100

    interrupt handler for pit

--- kernel/src/modules/interrupts/interrupts.zig
+++ kernel/src/modules/interrupts/interrupts.zig
@@ -1,16 +1,19 @@
 const ModuleSpec = @import("../module.zig").ModuleSpec;
 const Interrupts = @import("idt.zig");
 const Procedures = @import("../procedures.zig");
+const APIC = @import("../apic/apic.zig");
+const LAPIC = @import("../apic/local_apic.zig");
+const PIT = @import("../time/pit.zig");
 
 const std = @import("std");
 
 pub const ExceptionsModule = ModuleSpec{
     .name = "Exceptions",
-    .init = init,
+    .init = init_exceptions,
     .deinit = null,
 };
 
-pub fn init() void {
+pub fn init_exceptions() void {
     for (0..0xF) |i| {
         Interrupts.add_interrupt(@intCast(i), Interrupts.GateType.Interrupt, default_handler);
     }
@@ -29,6 +32,18 @@ pub fn default_handler(frame: *Interrupts.Frame) void {
 
 pub const InterruptsModule = ModuleSpec{
     .name = "Interrupts",
-    .init = null,
+    .init = init_int,
     .deinit = null,
 };
+
+pub fn init_int() void {
+    Interrupts.add_interrupt(0x20, Interrupts.GateType.Interrupt, pit_handler);
+    PIT.initPIT();
+
+    asm volatile ("sti");
+}
+
+pub fn pit_handler(f: *Interrupts.Frame) void {
+    Procedures.write_fmt("Test {}!\n", .{f}) catch {};
+    APIC.LAPIC_REF.?.writeRegister(LAPIC.REG.EOI, 0);
+}
