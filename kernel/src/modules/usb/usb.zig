f7999bea6630fbb6a9438620f8c602c3858afb24
Sawcce <sawcce@proton.me>
2024-01-16 20:21:45 +0100

    usb: basic usb detection

--- kernel/src/modules/usb/usb.zig
+++ kernel/src/modules/usb/usb.zig
@@ -1,7 +1,30 @@
 const ModuleSpec = @import("../module.zig").ModuleSpec;
+const Procedures = @import("../procedures.zig");
+const PCI = @import("../pci/pci.zig");
 
 pub const Module = ModuleSpec{
     .name = "USB Support",
-    .init = null,
+    .init = init,
     .deinit = null,
+};
+
+pub fn init() void {
+    const functions = PCI.getFunctions();
+    for (functions) |function_wrapper| {
+        const function = function_wrapper.function_ptr;
+        if (function.class_code & 0xFFFF00 != 0x0C0300) continue;
+
+        const usb_type: USBType = @enumFromInt(function.class_code & 0xFF);
+        Procedures.write_fmt("USB Type: {}\n", .{usb_type}) catch {};
+    }
+}
+
+const USBType = enum(u32) {
+    UHCI = 0,
+    OHCI = 0x10,
+    EHCI = 0x20,
+    XHCI = 0x30,
+    USB4 = 0x40,
+    Unspecified = 0x80,
+    USBDevice = 0xFE,
 };
